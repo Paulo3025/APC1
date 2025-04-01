6328ba64e006dcddee4df1caa6d56d63d1144a2c
Paulo Firmino de Freitas <paulo.freitasf3@gmail.com>
2025-04-01 10:17:18 -0300

    Adicionando arquivo da pratica3

---
+++ praticas/praticas3/declara_aluno.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main (){
+    // declarar uma variavel int  matricula com 8 digitos.
+    int matricula = 00000000;
+    printf("\nMatricula : %08i\n", matricula);
+
+    // declara umavariavel do tipo char para idade do estudante = 99 anos.
+    char idade = 99;
+    printf("Idade : %i anos\n", idade );
+
+    // declarar uma varivel do tipo float para altura = 9.90 m.
+    float altura = 9.90;
+    printf("Altura : %.2f m\n", altura);
+
+    // declarar umavariavel do tipo float para peso = 999.0 Kg.
+    float peso = 999.0;
+    printf("Peso : %.2f Kg\n", peso);
+
+    // declarar uma variavel do tipo char para sexo = F.
+   const  char sexo = 'F';
+   printf("Sexo : %c\n", sexo);
+
+    return 0;
+}
---
+++ praticas/praticas3/declara_constantes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define PI 3.141593// declara uma constante PI.
+
+
+int main(){
+    printf("\nValor de PI    : %.2f\n", PI);
+
+    const char SEXO_MASCULINO = 'M';
+    printf("Sexo Masculino : '%c'\n", SEXO_MASCULINO);
+
+    return 0;
+}
