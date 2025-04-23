2fa3f12fb2b382458afeaac57ba7f11a4e2cb2d2
unknown <2512082025@ACADEMICO.IESB.BR>
2025-04-23 10:53:00 -0300

    Adicionando arquivos da pratica06

---
+++ praticas/praticas6/estrutura_switch.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (){
+
+    int nota;
+    printf("Entre com sua nota : \n");
+    scanf("%i",&nota);
+
+    switch(nota) {
+        case 1: printf("Voce ganhou '*'.\n");break;
+        case 2: printf("Voce ganhou '**'.\n");break;
+        case 3: printf("Voce ganhou '***'.\n");break;
+        case 4: printf("Voce ganhou ****!\n");break;
+        case 5: printf("Voce ganhou *****!\n");break;
+        default: printf("Nota invalida! Tente novamente.\n");
+        
+    return 0;
+}
